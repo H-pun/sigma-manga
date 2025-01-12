d8e67323f8b1243c92bcba2a5c4f3f713909401b
Harvan Nurluthfi <harvan.nurluthfi@gmail.com>
2025-01-13 03:39:28 +0700

    refactor: decentralize datatable

--- src/app/dashboard/genre/page.tsx
+++ src/app/dashboard/genre/page.tsx
@@ -1,35 +1,111 @@
 "use client";
 
-import { fetchGenres } from "@/lib/data";
-import { DataTable } from "@/components/ui/data-table";
+import {
+  ColumnFiltersState,
+  SortingState,
+  getCoreRowModel,
+  getSortedRowModel,
+  useReactTable,
+  VisibilityState,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  PaginationState,
+} from "@tanstack/react-table";
 
-import { columns } from "./columns";
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { DataTable } from "@/components/ui/data-table";
+import { DataTablePagination } from "@/components/ui/data-table-pagination";
+
+import { ChevronDown } from "lucide-react";
+
+import { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 
-export default function Page() {
-  // const data = fetchGenres("");
-  // const debouncedSearch = useMemo(
-  //   () =>
-  //     debounce(async (searchQuery: string) => {
-  //       if (!searchQuery) return;
-  //       await fetchGenres(searchQuery);
-  //     }, 500),
-  //   [],
-  // );
+import { fetchGenres } from "@/lib/data";
+import { columns } from "./columns";
 
-  // const handleSearch = async (value: string) => {
-  //   setQuery(value);
-  //   debouncedSearch(value);
-  // }
+export default function Page() {
+  const [sorting, setSorting] = useState<SortingState>([]);
+  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
+  const [columnVisibility, setColumnVisibility] = useState<VisibilityState>({});
+  const [pagination, setPagination] = useState<PaginationState>({
+    pageIndex: 0,
+    pageSize: 5,
+  });
 
   const { data } = useQuery({
     queryKey: ["genre"],
     queryFn: () => fetchGenres(""),
   });
 
+  const table = useReactTable({
+    data: data || [],
+    columns,
+    onSortingChange: setSorting,
+    onColumnFiltersChange: setColumnFilters,
+    onColumnVisibilityChange: setColumnVisibility,
+    onPaginationChange: setPagination,
+    getCoreRowModel: getCoreRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+
+    state: {
+      sorting,
+      columnFilters,
+      columnVisibility,
+      pagination,
+    },
+  });
+
   return (
     <div className="container p-4">
-      <DataTable columns={columns} data={data || []} />
+      <div className="flex items-center py-4">
+        <Input
+          placeholder="Filter genre..."
+          value={(table.getColumn("name")?.getFilterValue() as string) ?? ""}
+          onChange={(event) =>
+            table.getColumn("name")?.setFilterValue(event.target.value)
+          }
+          className="max-w-sm"
+        />
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline" className="ml-auto">
+              Columns <ChevronDown />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            {table
+              .getAllColumns()
+              .filter((column) => column.getCanHide())
+              .map((column) => {
+                return (
+                  <DropdownMenuCheckboxItem
+                    key={column.id}
+                    className="capitalize"
+                    checked={column.getIsVisible()}
+                    onCheckedChange={(value) =>
+                      column.toggleVisibility(!!value)
+                    }
+                  >
+                    {column.id}
+                  </DropdownMenuCheckboxItem>
+                );
+              })}
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+      <DataTable columns={columns} table={table} />
+      <DataTablePagination table={table} />
     </div>
   );
 }
--- src/app/dashboard/manga/page.tsx
+++ src/app/dashboard/manga/page.tsx
@@ -1,20 +1,111 @@
 "use client";
 
-import { fetchMangas } from "@/lib/data";
-import { DataTable } from "@/components/ui/data-table";
+import {
+  ColumnFiltersState,
+  SortingState,
+  getCoreRowModel,
+  getSortedRowModel,
+  useReactTable,
+  VisibilityState,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  PaginationState,
+} from "@tanstack/react-table";
 
-import { columns } from "./columns";
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { DataTable } from "@/components/ui/data-table";
+import { DataTablePagination } from "@/components/ui/data-table-pagination";
+
+import { ChevronDown } from "lucide-react";
+
+import { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 
+import { fetchMangas } from "@/lib/data";
+import { columns } from "./columns";
+
 export default function Page() {
+  const [sorting, setSorting] = useState<SortingState>([]);
+  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
+  const [columnVisibility, setColumnVisibility] = useState<VisibilityState>({});
+  const [pagination, setPagination] = useState<PaginationState>({
+    pageIndex: 0,
+    pageSize: 5,
+  });
+
   const { data } = useQuery({
-    queryKey: ["manga"],
+    queryKey: ["genre"],
     queryFn: () => fetchMangas(""),
+  });
+
+  const table = useReactTable({
+    data: data || [],
+    columns,
+    onSortingChange: setSorting,
+    onColumnFiltersChange: setColumnFilters,
+    onColumnVisibilityChange: setColumnVisibility,
+    onPaginationChange: setPagination,
+    getCoreRowModel: getCoreRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+
+    state: {
+      sorting,
+      columnFilters,
+      columnVisibility,
+      pagination,
+    },
   });
 
   return (
     <div className="container p-4">
-      <DataTable columns={columns} data={data || []} />
+      <div className="flex items-center py-4">
+        <Input
+          placeholder="Filter manga..."
+          value={(table.getColumn("name")?.getFilterValue() as string) ?? ""}
+          onChange={(event) =>
+            table.getColumn("name")?.setFilterValue(event.target.value)
+          }
+          className="max-w-sm"
+        />
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline" className="ml-auto">
+              Columns <ChevronDown />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            {table
+              .getAllColumns()
+              .filter((column) => column.getCanHide())
+              .map((column) => {
+                return (
+                  <DropdownMenuCheckboxItem
+                    key={column.id}
+                    className="capitalize"
+                    checked={column.getIsVisible()}
+                    onCheckedChange={(value) =>
+                      column.toggleVisibility(!!value)
+                    }
+                  >
+                    {column.id}
+                  </DropdownMenuCheckboxItem>
+                );
+              })}
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+      <DataTable columns={columns} table={table} />
+      <DataTablePagination table={table} />
     </div>
   );
 }
