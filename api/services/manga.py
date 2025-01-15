f6e5679b2285048a6a785c8668ab27e7fe388378
Harvan Nurluthfi <harvan.nurluthfi@gmail.com>
2025-01-16 02:33:43 +0700

    feat: genre filter

--- api/services/manga.py
+++ api/services/manga.py
@@ -38,11 +38,18 @@ async def get_all_mangas(db: Session, *, filter: FilterParams):
         .join(assoc_manga_genre, Manga.id == assoc_manga_genre.c.id_manga)
         .join(Genre, Genre.id == assoc_manga_genre.c.id_genre)
         .group_by(Manga.id)
-        # .order_by(Manga.updated_at.desc())
+        # .order_by(Manga.title.desc())
     )
-
     if filter.search:
-        query = query.filter(Manga.title.ilike(f"%{filter.search}%"))
+        if filter.search.startswith("genre_ids:"):
+            genre_param = filter.search.split(":")[1].split(";")
+            genre_ids = genre_param[0].split(",")
+            query = query.filter(Manga.genres.any(Genre.id.in_(genre_ids)))
+            print(len(genre_param))
+            if len(genre_param) > 1:
+                query = query.filter(Manga.title.ilike(f"%{genre_param[1]}%"))
+        else:
+            query = query.filter(Manga.title.ilike(f"%{filter.search}%"))
     return Pagination.from_query(DetailManga, query, filter)
 
 
