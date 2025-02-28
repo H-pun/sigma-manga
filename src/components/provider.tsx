8a4425b51484c71e7d58515dfa649893709add05
Harvan Nurluthfi <harvan.nurluthfi@gmail.com>
2025-02-28 10:46:38 +0700

    feat: configure Highcharts options to disable credits on client-side

--- src/components/provider.tsx
+++ src/components/provider.tsx
@@ -1,9 +1,20 @@
 "use client";
 
+import { useEffect } from "react";
+import Highcharts from "highcharts";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ThemeProvider } from "@/components/theme-provider";
 
 const Provider = ({ children }: { children: React.ReactNode }) => {
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      Highcharts.setOptions({
+        credits: {
+          enabled: false,
+        },
+      });
+    }
+  }, []);
   const queryClient = new QueryClient();
   return (
     <QueryClientProvider client={queryClient}>
